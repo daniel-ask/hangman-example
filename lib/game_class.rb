60d34f31c19bbb0fb8a380175a15f1dcccdc62a8
Daniel <dask@CA-EDU01.local>
2021-03-23 14:07:09 +1100

    Added run loop and letter guessing functionality

---
+++ lib/game_class.rb
@@ -0,0 +1,61 @@
+class Game
+ def initialize
+  @words = ['test','consider','implications','then','move','fast','and','break','things']
+  @word = @words.sample
+  @lives = @word.length + 2
+  @guessed_letters = []
+ end
+
+ def run
+  loop do
+    display_word
+    display_lives
+    display_instructions
+    process_input(user_input)
+  end
+ end
+
+ def display_word
+  @word.chars.each do |char|
+    if @guessed_letters.include?(char)
+      print "#{char.upcase} "
+    else
+      print '_ '
+    end
+  end
+  puts
+ end
+
+ def display_lives
+  puts "You have #{@lives} lives left"
+ end
+
+ def display_instructions
+  puts 'Enter a letter or your guess:'
+ end
+
+ def user_input
+  gets.strip.downcase
+ end
+
+ def process_input(input)
+  if input.length <= 1
+    process_letter(input)
+  else
+    process_guess(input)
+  end
+ end
+
+ def process_letter(letter)
+  @guessed_letters << letter
+  update_lives(letter)
+ end
+
+ def process_guess(guess)
+
+ end
+
+ def update_lives(letter)
+    @lives -= 1 unless @word.include?(letter)
+ end
+end
