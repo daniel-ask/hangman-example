e83af81008044c73a141a46abb5351c560c60b54
Daniel Ask <cdanielask@gmail.com>
2021-03-25 14:47:47 +1100

    Reformated code with Rubocop

--- lib/game_class.rb
+++ lib/game_class.rb
@@ -1,102 +1,103 @@
 class Game
-
- def initialize(word)
-  @word = word 
-  @lives = @word.length + 2
-  @guessed_letters = []
-  @game_state = true
- end
-
- def run
-  while @game_state
-    system 'clear'
-    line_seperator
-    display_word
-    display_lives
-    display_guessed_letters
-    line_seperator
-    display_instructions
-    process_input(user_input)
+  def initialize(word)
+    @word = word
+    @lives = @word.length + 2
+    @guessed_letters = []
+    @game_state = true
   end
- end
 
- def line_seperator
-  puts "<>"* 10
- end
-
- def display_guessed_letters
-  puts "Guessed letters:"
-  @guessed_letters.each do |letter|
-    print "#{letter.upcase} "
-  end
-  puts
- end
-
- def display_word
-  @word.chars.each do |char|
-    if @guessed_letters.include?(char.downcase) || !char.downcase.match?(/[a-z]/) 
-      print "#{char} "
-    else
-      print '_ '
+  def run
+    while @game_state
+      system 'clear'
+      line_seperator
+      display_word
+      display_lives
+      display_guessed_letters
+      line_seperator
+      display_instructions
+      process_input(user_input)
     end
   end
-  puts
- end
 
- def display_lives
-  puts "You have #{@lives} lives left"
- end
-
- def display_instructions
-  puts 'Enter a letter or your guess:'
- end
-
- def user_input
-  gets.strip.downcase
- end
-
- def process_input(input)
-  if input.length > 1 
-    process_guess(input)
-  elsif !(input.match?(/^[a-z]+$/)) || @guessed_letters.include?(input)
-    puts 'Invalid Input'
-    sleep 1
-  else
-    process_letter(input)
+  def line_seperator
+    puts '<>' * 10
   end
-end
 
- def process_letter(letter)
-  @guessed_letters << letter
-  update_lives(letter)
-  check_lives
- end
+  def display_guessed_letters
+    puts 'Guessed letters:'
+    @guessed_letters.each do |letter|
+      print "#{letter.upcase} "
+    end
+    puts
+  end
 
- def process_guess(guess)
-  if @word.downcase == guess.downcase
-    win
-  else
+  def display_word
+    @word.chars.each do |char|
+      if @guessed_letters.include?(char.downcase) || !char.downcase.match?(/[a-z]/)
+        print "#{char} "
+      else
+        print '_ '
+      end
+    end
+    puts
+  end
+
+  def display_lives
+    puts "You have #{@lives} lives left"
+  end
+
+  def display_instructions
+    puts 'Enter a letter or your guess:'
+  end
+
+  def user_input
+    gets.strip.downcase
+  end
+
+  def process_input(input)
+    if input.length > 1
+      process_guess(input)
+    elsif !input.match?(/^[a-z]+$/) || @guessed_letters.include?(input)
+      puts 'Invalid Input'
+      sleep 1
+    else
+      process_letter(input)
+    end
+  end
+
+  def process_letter(letter)
+    @guessed_letters << letter
+    update_lives(letter)
+    check_lives
+  end
+
+  def process_guess(guess)
+    if @word.downcase == guess.downcase
+      win
+    else
+      game_over
+    end
+  end
+
+  def update_lives(letter)
+    @lives -= 1 unless @word.include?(letter)
+  end
+
+  def check_lives
+    return unless @lives <= 0
+
     game_over
   end
- end
 
- def update_lives(letter)
-    @lives -= 1 unless @word.include?(letter)
- end
+  def game_over
+    # Guard clause
+    puts 'GAME OVER'
+    puts "The word was: #{@word}"
+    @game_state = false
+  end
 
- def check_lives
-  return unless @lives <= 0
-  game_over
- end
- def game_over
-  # Guard clause
-   puts 'GAME OVER'
-   puts "The word was: #{@word}"
-   @game_state = false
- end
- 
- def win
-  puts 'YOU WON CONGRATS'
-  @game_state = false
- end
+  def win
+    puts 'YOU WON CONGRATS'
+    @game_state = false
+  end
 end
--- lib/hangman_class.rb
+++ lib/hangman_class.rb
@@ -10,60 +10,61 @@ class Hangman
     end
   end
 
- def display_welcome
-  puts '-' * 20
-  puts 'WELCOME TO HANGMAN'
-  puts '-' * 20
- end
-
- def display_menu
-  puts '1.  Play game'
-  puts '2.  Exit'
- end
-
- def display_categories
-  puts 'Choose your category:'
-  puts "\t1. Movie"
-  puts "\t2. Game"
-  puts "\t3. Programming Language"
- end
-
- def menu_input
-  gets.to_i
- end
-
- def process_input(menu_selection)
-  case menu_selection
-  when 1
-    initiate_game 
-  when 2
-    exit_game
+  def display_welcome
+    puts '-' * 20
+    puts 'WELCOME TO HANGMAN'
+    puts '-' * 20
   end
- end
 
- def initiate_game
-  # get category selection
-  display_categories
-  menu_selection = menu_input
-  # generate word
-  word = generate_word(menu_selection)
-  # start game
-  game = Game.new(word)
-  game.run
- end
-
- def generate_word(menu_selection)
-  case menu_selection
-  when 1
-    Faker::Movie.title
-  when 2
-    Faker::Game.title
-  when 3
-    Faker::ProgrammingLanguage.name
+  def display_menu
+    puts '1.  Play game'
+    puts '2.  Exit'
   end
- end
- def exit_game
-  puts 'Good Bye'
-  exit
- end
+
+  def display_categories
+    puts 'Choose your category:'
+    puts "\t1. Movie"
+    puts "\t2. Game"
+    puts "\t3. Programming Language"
+  end
+
+  def menu_input
+    gets.to_i
+  end
+
+  def process_input(menu_selection)
+    case menu_selection
+    when 1
+      initiate_game
+    when 2
+      exit_game
+    end
+  end
+
+  def initiate_game
+    # get category selection
+    display_categories
+    menu_selection = menu_input
+    # generate word
+    word = generate_word(menu_selection)
+    # start game
+    game = Game.new(word)
+    game.run
+  end
+
+  def generate_word(menu_selection)
+    case menu_selection
+    when 1
+      Faker::Movie.title
+    when 2
+      Faker::Game.title
+    when 3
+      Faker::ProgrammingLanguage.name
+    end
+  end
+
+  def exit_game
+    puts 'Good Bye'
+    exit
+  end
 end
